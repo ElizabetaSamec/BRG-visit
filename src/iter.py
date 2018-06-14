1d50f14fd71501a3523c4699c0d29b0e85bd04bf
ElizabetaSamec <33370081+ElizabetaSamec@users.noreply.github.com>
2018-06-14 09:27:13 +0200

    version02

--- src/iter.py
+++ src/iter.py
@@ -921,7 +921,7 @@ def multistepFDM_inexact(nodes, elems, supports, qs, fcs=[], lcs=[], l0cs=[],
     return (nc, f, qs0)
 
 ###############################
-#without lo in optimisation
+#without l0 in optimisation
 ###############################
 
 # def multistepFDM_inexact(vertices, edges, fixed, qs, fcs=[], lcs=[], l0cs=[],
@@ -1116,18 +1116,17 @@ if __name__ == '__main__':
     import compas
 
     from compas.datastructures import Network
-    from compas.plotters import NetworkPlotter
+    from compas.viewers import NetworkViewer
 
-    network = Network.from_obj(compas.get('simple1.obj'))
+    from compas.numerical import fd_numpy
 
-    dva = {'is_anchor': False}
+    network = Network.from_obj(compas.get('saddle.obj'))
+
+    dva = {'is_anchor': False, 'px': 0.0, 'py': 0.0, 'pz': 0.0}
     dea = {'q': 1.0}
 
     network.update_default_vertex_attributes(dva)
     network.update_default_edge_attributes(dea)
-
-    # identify *anchored* vertices
-
 
     for key in network.vertices():
         network.vertex[key]['is_anchor'] = network.is_vertex_leaf(key)
@@ -1135,17 +1134,20 @@ if __name__ == '__main__':
     key_index = network.key_index()
 
     xyz = network.get_vertices_attributes(('x', 'y', 'z'))
+    loads = network.get_vertices_attributes(('px', 'py', 'pz'))
+    q = network.get_edges_attribute('q')
     fixed = network.vertices_where({'is_anchor': True})
     fixed = [key_index[key] for key in fixed]
     edges = [(key_index[u], key_index[v]) for u, v in network.edges()]
-    q = network.get_edges_attribute('q')
 
+    xyz = FDM(xyz, edges, fixed, q)
 
-    indexf = get_index(edges, edges)
-    fs = map_value_to_all_edges(edges, 1.0)
-    force_constraints = edge_constraints(indexf, fs)
+    for key, attr in network.vertices(True):
+        index = key_index[key]
+        attr['x'] = xyz[index, 0]
+        attr['y'] = xyz[index, 1]
+        attr['z'] = xyz[index, 2]
 
-    nc, f, q = multistepFDM(xyz, edges, fixed, q, fcs=force_constraints, steps=1000)
-    l = list_of_element_lengths(edges, nc)
-
-    print (f)
+    viewer = NetworkViewer(network)
+    viewer.setup()
+    viewer.show()
